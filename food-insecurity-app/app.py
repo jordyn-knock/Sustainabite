13621870a99ab4420b998e93257defc36d2489e8
Hannah <hkamarul@student.ubc.ca>
2025-04-05 13:39:35 -0700

    more edits yay

--- food-insecurity-app/app.py
+++ food-insecurity-app/app.py
@@ -1,8 +1,19 @@
 import streamlit as st
+from pantry_tab import render_pantry_tab
+from favourites_tab import render_favourites_tab
 from PIL import Image
+
 #this is the main page 
 st.title("I'm Hungry")
 st.write("Upload an image of your ingredients and get a recipe!") # or whatever you want to write here
+
+# Define your tabs
+tab1, tab2 = st.tabs(["Pantry & My List", "Favourites"])
+
+with tab1:
+    render_pantry_tab()
+with tab2:
+    render_favourites_tab()
 
 uploaded_files = st.file_uploader( #this is where the user uploads their images
     "Upload images of your ingredients", 
@@ -22,3 +33,4 @@ if uploaded_files:
         extracted = ["tomato", "garlic"]  #this is just an example!!!
         ingredient_list.extend(extracted)
         st.success("Files have successfully uploaded!")
+

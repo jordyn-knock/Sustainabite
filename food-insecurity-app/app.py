db26f517ea3df8c14cbf2c62f9645e8432cc945d
Hannah <hkamarul@student.ubc.ca>
2025-04-05 13:17:16 -0700

    made a couple more

--- food-insecurity-app/app.py
+++ food-insecurity-app/app.py
@@ -10,11 +10,15 @@ uploaded_files = st.file_uploader( #this is where the user uploads their images
     accept_multiple_files=True
 )
 
+ingredient_list = []
+
 if uploaded_files: 
     #for loop so that the Ai model can go through all the uploaded images
     for file in uploaded_files:
         image = Image.open(file)
-        st.image(image, caption="Your ingredients", use_column_width=True)
+        st.image(image, caption="Your ingredients",  use_container_width=True)
         st.write("We're processing your information")
         # This is where we call the AI model 
+        extracted = ["tomato", "garlic"]  #this is just an example!!!
+        ingredient_list.extend(extracted)
         st.success("Files have successfully uploaded!")

660192fcff9f4ebb7c42877e9531f7798691b9ab
Hannah <hkamarul@student.ubc.ca>
2025-04-05 13:05:36 -0700

    made edits to ui

--- food-insecurity-app/app.py
+++ food-insecurity-app/app.py
@@ -2,13 +2,19 @@ import streamlit as st
 from PIL import Image
 
 st.title("I'm Hungry")
-st.write("Upload an image of your ingredients and get a recipe!")
+st.write("Upload an image of your ingredients and get a recipe!") # or whatever you want to write here
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_files = st.file_uploader( #this is where the user uploads their images
+    "Upload images of your ingredients", 
+    type=["jpg", "jpeg", "png"], 
+    accept_multiple_files=True
+)
 
-if uploaded_file:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Your ingredients", use_column_width=True)
-    st.write("Analyzing...")
-    # Placeholder for AI model result
-    st.success("This is where your recipe will appear!")
+if uploaded_files: 
+    #for loop so that the Ai model can go through all the uploaded images
+    for file in uploaded_files:
+        image = Image.open(file)
+        st.image(image, caption="Your ingredients", use_column_width=True)
+        st.write("We're processing your information")
+        # This is where we call the AI model 
+        st.success("Files have successfully uploaded!")
